6b26dc5174e420f30d4c65148586fd6d66ca40e8
Rolando Velazquez <velazquezchirolando@gmail.com>
2020-07-09 21:19:18 -0500

    antes de las pruebas

--- public/js/app.js
+++ public/js/app.js
@@ -43750,8 +43750,8 @@ if (document.getElementById('TabRazas')) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\www\laravel-react\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\www\laravel-react\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\grupo-manee\laravel-react\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\grupo-manee\laravel-react\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
